1394e3c4751d1e74ec08a308d0f81e433f8606ff
ziming <guoaihua@geetest.com>
2022-08-25 23:20:05 +0800

    feat: init repo

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const Koa = require("koa");
+const { createClient } = require("redis");
+const app = new Koa();
+const path = require("path");
+const views = require("koa-views");
+const router = require("./route");
+
+const client = createClient({});
+client.connect();
+client.on("error", (err) => console.log("Redis Client Error", err));
+
+app.context.redis = client;
+
+// 加载模板引擎
+app.use(
+  views(path.join(__dirname, "./view"), {
+    extension: "ejs",
+  })
+);
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(3000);
+
+
+
---
+++ src/route/index.js
@@ -0,0 +1,15 @@
+const Router = require('@koa/router');
+const router = new Router()
+
+router.get('/(.*)', async (ctx)=>{
+
+    const modules = await ctx.redis.get('modules')
+    console.log(modules)
+    let title = 'hello koa2'
+    await ctx.render('index', {
+      title,
+      modules: JSON.parse(modules)
+    })
+})
+
+module.exports = router
